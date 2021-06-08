02b747d97d5549f8b03fa86dd981c4e16e20aa48
Joel Gustafson <joelg@mit.edu>
2021-06-08 11:26:59 -0700

    delete lib

--- lib/reduce.d.ts
+++
@@ -1,3 +0,0 @@
-import type { EditorState, Kinds, Schema } from "./state.js";
-import { EditorAction } from "./actions.js";
-export declare function reduce<S extends Schema>(kinds: Kinds<S>, state: EditorState<S>, action: EditorAction<S>): EditorState<S>;
--- lib/viewer/ReadonlyCanvas.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import type { EditorState, Kinds, Schema } from "../state.js";
-import { FocusAction } from "../actions.js";
-export interface ReadonlyCanvasProps<S extends Schema> {
-    kinds: Kinds<S>;
-    state: EditorState<S>;
-    dispatch: (action: FocusAction) => void;
-}
-export declare function Canvas<S extends Schema>(props: ReadonlyCanvasProps<S>): JSX.Element;
